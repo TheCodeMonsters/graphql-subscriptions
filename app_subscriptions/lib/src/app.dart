e3775a74755dba42e51d93a214d2c73391941282
Manuel Duarte <carlosd99m@gmail.com>
2022-02-09 21:58:01 -0600

    chore(app): consume the data with grahql_flutter (#2)
    * docs: readme create
    
    * :heavy_plus_sign: add resolvers
    
    * chore: project app setup
    
    * chore: graphql mutations

---
+++ app_subscriptions/lib/src/app.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+import 'ui/screens/home_page.dart';
+
+final HttpLink httpLink = HttpLink(
+  'http://localhost:8080/',
+);
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  ValueNotifier<GraphQLClient> client = ValueNotifier(
+    GraphQLClient(
+      cache: GraphQLCache(),
+      link: httpLink,
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return GraphQLProvider(
+      client: client,
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Books',
+        home: HomePage(),
+      ),
+    );
+  }
+}
---
+++ app_subscriptions/lib/src/models/books.dart
@@ -0,0 +1,23 @@
+class Books {
+  static String getAllBooks = """ 
+      query getAllBooks {
+        books{
+          title
+          author
+          published
+        }
+      } 
+    """;
+
+  static String mutationAdd = """
+    mutation {
+      createBook (title: "Hola", author: "Hola", published: 2022, genres: []){
+        title: title
+        author: author
+        published: published
+        genres:genres
+      }
+    }
+  
+  """;
+}
